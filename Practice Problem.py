3f0c7ba871337aacfa5a8ed2fff8ed0e4f87094c
ChristianDalePatena <qcdsjpatena@tip.edu.ph>
2024-08-30 16:38:29 +0800

    Add files via upload

---
+++ Practice Problem.py
@@ -0,0 +1,25 @@
+def score_given():
+   for score in given_scores:
+       if score % 2 == 0:
+           even_score.append(score)
+       else:
+           pass
+             
+            
+given_scores = [26, 49, 98, 87, 62, 75]
+even_score = []
+
+score_given()         
+print(even_score)
+print(sum(even_score))
+           
+ 
+           
+           
+    
+           
+          
+
+            
+            
+            
