4f56bb346b2d229654ea4074e561a0bce6414d8c
ChristianDalePatena <qcdsjpatena@tip.edu.ph>
2024-11-13 17:02:44 +0800

    Add files via upload

---
+++ Exercise_6/Exercise_6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void display (queue<string>pl)
+{
+    while(!pl.empty())
+    {
+    cout<<pl.front()<<"\n";
+    pl.pop();
+    }
+}
+
+
+int main()
+{
+    queue <string> pl;
+    pl.push("c++ ");
+    pl.push("Java ");
+    pl.push("Python ");
+    cout<<"Initial queue: "<<endl;
+    display (pl);
+    pl.pop();
+    pl.pop();
+    cout<<"\n"<<"Final queue: "<<endl;
+    display (pl);
+    return 0;
+}
